1a492c9fb2fb4fce040c7731c4e52d8565ad30c1
Vladislav <Vladislav@FENIX-PC>
2024-04-21 09:33:25 +0300

    Добавьте файлы проекта.

---
+++ HW-03/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delete();
+        }
+
+        static public void Delete()
+        {
+
+
+            string FileDir = @"/Users/Vladislav.Fenix-PC/Desktop/test";
+
+            try
+            {
+                if (Directory.Exists(FileDir))
+                {
+                    DirectoryInfo DirInfo = new DirectoryInfo(FileDir);
+
+                    DateTime LastDate = DirInfo.LastAccessTime;
+                    Console.WriteLine("Directories: ");
+
+                    Console.WriteLine();
+
+                    foreach (var Direct in DirInfo.GetDirectories())
+                    {
+                        Console.WriteLine(Direct.Name);
+                        if ((DateTime.Now - LastDate) > TimeSpan.FromMinutes(1))
+                        {
+                            Console.WriteLine("Directory {0} has delete", Direct.Name);
+                            Direct.Delete(true);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Time < 30 minuts");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    Console.WriteLine("Folder: ");
+
+                    Console.WriteLine();
+
+                    foreach (var File in DirInfo.GetFiles())
+                    {
+                        Console.WriteLine(File.Name);
+                        if ((DateTime.Now - LastDate) > TimeSpan.FromMinutes(1))
+                        {
+                            Console.WriteLine("File {0} has Delete", File.Name);
+                            File.Delete();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Time < 30 minuts");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
